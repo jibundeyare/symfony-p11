5762ff521a51f52e3b97f67e4a818e6975dab1bf
jibundeyare <contact@jibundeyare.com>
2023-09-22 17:52:51 +0200

    Add webpack encore

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
